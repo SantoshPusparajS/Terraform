3c8ae8180a225a70d9303a2561899a997d007515
Santosh Pusparaj S <crowinlearnings@gmail.com>
2023-12-03 11:57:48 +0530

    Added a file in 04-meta-arguments for_each demo

--- Azure Terraform/04-Meta-Arguments/c2-virtual-network.tf
+++ Azure Terraform/04-Meta-Arguments/c2-virtual-network.tf
@@ -1,9 +1,3 @@
-#Creating resource group
-resource "azurerm_resource_group" "rg1" {
-  name     = "my-resource-group-1"
-  location = "East US"
-}
-
 #creating virtual network
 resource "azurerm_virtual_network" "vnet01" {
   name                = "sample-vnet-01"
---
+++ Azure Terraform/04-Meta-Arguments/c4-resource-group.tf
@@ -0,0 +1,20 @@
+#Creating resource group
+#for_each demo with map
+resource "azurerm_resource_group" "rg" {
+  for_each = {
+    dcapps1 = "eastus"
+    dcapps2 = "eastus2"
+    dcapps3 = "westus"
+  }
+  name     = "${each.key}-resource-group"
+  location = each.value
+}
+
+#creating resource group
+#for_each demo with set
+
+# resource "azurerm_resource_group" "rg" {
+#   for_each = toset(["eastus", "eastus2", "westus"])
+#   name     = "${each.key}-resource-group"
+#   location = each.key
+# }
